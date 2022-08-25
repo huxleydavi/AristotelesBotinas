a0f4e6032cc6e65380573421df38f90a0b3067ab
Hx <92643240+hscorpooration@users.noreply.github.com>
2022-08-25 15:55:01 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 //--------------------------By HS Corp-------------------------------//
 //---------------------------@hs.corp-------------------------------//
 //-----------------------hscorpp@gmail.com-------------------------------//
-$('.mobile > i').click(function(){$(this).parent().find('ul').slideToggle();});
+/*Menu mobile*/$('.mobile > i').click(function(){$(this).parent().find('ul').slideToggle();});/*Mascara do telefone no contato -------*/function mascara(o,f){v_obj=o v_fun=f setTimeout("execmascara()",1)}function execmascara(){v_obj.value=v_fun(v_obj.value)}function mtel(v){v=v.replace(/\D/g,""); //Remove tudo o que não é dígito v=v.replace(/^(\d{2})(\d)/g,"($1) $2"); //Coloca parênteses em volta dos dois primeiros dígitos v=v.replace(/(\d)(\d{4})$/,"$1-$2"); //Coloca hífen entre o quarto e o quinto dígitos return v;}function id( el ){return document.getElementById( el );}window.onload=function(){id('telefone').onkeyup=function(){mascara( this, mtel );}};
